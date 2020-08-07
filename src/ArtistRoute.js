0d09fd41c4a5e3063e6b1d130b5faa0028463682
Jordan Gabay <gabay.jordan@outlook.com>
2020-08-06 21:52:14 -0400

    Exercise 6 Complete

--- src/ArtistRoute.js
+++ src/ArtistRoute.js
@@ -1,7 +1,11 @@
 import React from "react";
+import {useSelector} from 'react-redux';
 
 function ArtistRoute() {
-  return <div>Artist: Logic</div>;
+    const accessToken = useSelector((state) => state.auth.token)
+  return(
+      <div>{accessToken}</div>
+  );
 }
 
 export default ArtistRoute;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import GlobalStyles from '../GlobalStyles';
 import ArtistRoute from '../../ArtistRoute'
+import {useDispatch} from 'react-redux';
+import {requestAccessToken, receiveAccessToken, receiveAccessTokenError} from '../actions'
 
 const DEFAULT_ARTIST_ID = "4xRYI6VqpkE3UwrDrAZL8L";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(requestAccessToken());
+
+    fetch("/spotify_access_token")
+      .then((response) => response.json())
+      .then((data) => dispatch(receiveAccessToken(data.access_token)))
+      .catch((error) => dispatch(receiveAccessTokenError()));
+  }, []);
+  
   return <Router>
     <GlobalStyles/>
     <Switch>
