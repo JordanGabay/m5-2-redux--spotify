862c6e32761afe16291f7f9e3200a8a3e44546f2
Jordan Gabay <gabay.jordan@outlook.com>
2020-08-06 21:43:28 -0400

    Exercise 5 Complete

---
+++ src/ArtistRoute.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ArtistRoute() {
+  return <div>Artist: Logic</div>;
+}
+
+export default ArtistRoute;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import GlobalStyles from '../GlobalStyles';
+import ArtistRoute from '../../ArtistRoute'
+
+const DEFAULT_ARTIST_ID = "4xRYI6VqpkE3UwrDrAZL8L";
 
 const App = () => {
   return <Router>
     <GlobalStyles/>
-    TODO
+    <Switch>
+      <Route exact path='/artists/:id'>
+        <ArtistRoute/>
+      </Route>
+      <Redirect from='/' to={`/artists/${DEFAULT_ARTIST_ID}`}/>
+    </Switch>
     </Router>;
 };
 
